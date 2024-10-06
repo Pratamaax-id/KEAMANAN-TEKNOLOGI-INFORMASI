3def443be8154237bbba883c84f16826fd947a9b
Pratamaax-id <andika121pratama@gmail.com>
2024-10-06 14:13:24 +0700

    Implementasi Registrasi dan Login

--- SampleSecureWeb/ViewModels/ChangePasswordViewModel.cs
+++ SampleSecureWeb/ViewModels/ChangePasswordViewModel.cs
@@ -7,17 +7,17 @@ namespace SampleSecureWeb.ViewModels
     {
         [Required]
         [DataType(DataType.Password)]
-        public string? CurrentPassword { get; set; }  // Jadikan nullable
+        public string? CurrentPassword { get; set; }  
 
         [Required]
         [MinLength(12, ErrorMessage = "Kata sandi baru harus memiliki panjang setidaknya 12 karakter.")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Kata sandi baru harus mengandung setidaknya satu huruf besar, satu huruf kecil, dan satu angka.")]
         [DataType(DataType.Password)]
-        public string? NewPassword { get; set; }  // Jadikan nullable
+        public string? NewPassword { get; set; } 
 
         [Required]
         [Compare("NewPassword", ErrorMessage = "Kata sandi tidak cocok.")]
         [DataType(DataType.Password)]
-        public string? ConfirmNewPassword { get; set; }  // Jadikan nullable
+        public string? ConfirmNewPassword { get; set; }  
     }
 }
